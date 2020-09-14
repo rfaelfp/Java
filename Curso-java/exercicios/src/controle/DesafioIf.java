69ca67216de4550bd2476f2455999156e59664cb
rfaelfp <54748929+rfaelfp@users.noreply.github.com>
2020-09-14 10:09:57 -0300

    14/09/2020

---
+++ Curso-java/exercicios/src/controle/DesafioIf.java
@@ -0,0 +1,15 @@
+package controle;
+
+public class DesafioIf {
+	
+	public static void main(String[] args) {
+		
+		double nota = 2.3;
+		
+		if (nota >= 9.0); {
+			System.out.println("Aprovado!"); // O bloco de código sem vinculação é válido em java.
+			System.out.println("Você é fera");
+		}
+	}
+
+}
